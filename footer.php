b9052110c12417669847f34915325810ca8d8e94
Alessandra Milburn <57661880+AlessandraMilburn@users.noreply.github.com>
2020-06-24 16:35:41 +0400

    added Github icon and link

--- footer.php
+++ footer.php
@@ -2,14 +2,15 @@
 <!-------------footer---------------->
 
 <footer>
-      <ul>
-        <a href="https://www.linkedin.com/in/alessandramilburn/"><img src="/wp-content/uploads/2020/03/LinkedIn.svg"  width="24" height="24"/></a>
-        <a href="mailto:alessandramilburn@yahoo.com"><img src="/wp-content/uploads/2020/04/Email.svg"  width="22" height="22"/></a>
-        <a href="https://www.instagram.com/am_codesigns/"><img src="/wp-content/uploads/2020/03/Instagram.svg"  width="22" height="22"/></a>
-      </ul>
+      <div class="social-menu">
+        <div><a href="https://www.linkedin.com/in/alessandramilburn/"><i class="fab fa-facebook-f"></i></a></div>
+        <div><a href="mailto:alessandramilburn@yahoo.com"><i class="fas fa-envelope"></i></a></div>
+        <div><a href="https://www.instagram.com/am_codesigns/"><i class="fab fa-linkedin-in"></i></a></div>
+        <div><a href="https://github.com/AlessandraMilburn"><i class="fab fa-github"></i></a></div>      
+      </div>
 
       <h4>
-        COPYRIGHT 2020 ALESSANDRA MILBURN
+        COPYRIGHT 2020 <wbr> ALESSANDRA MILBURN
       </h4>
     </footer>
   
