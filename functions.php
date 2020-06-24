75416a00a709e0ddbdab5f4b7ea08bf1c24a83aa
Alessandra Milburn <57661880+AlessandraMilburn@users.noreply.github.com>
2020-06-24 15:03:14 +0400

    organized code structure

--- functions.php
+++ functions.php
@@ -1,52 +1,64 @@
 <?php
+/*--------------------------------------------------------------
+>>> TABLE OF CONTENTS:
+----------------------------------------------------------------
+1.0 Theme Files
+2.0 Admin Features
+3.0 Meta Tags
+--------------------------------------------------------------*/
 
-  //FILES
-  function am_files() {
-    // load as many css and javascript files
 
-    //fonts
-    wp_enqueue_style('Fjalla One-font', '//fonts.googleapis.com/css?family=Fjalla+One&display=swap');
-    wp_enqueue_style('Caveat-font', '//fonts.googleapis.com/css?family=Caveat&display=swap');
-    wp_enqueue_style('Lato-font', '//fonts.googleapis.com/css?family=Lato:300,400&display=swap'); 
+/*--------------------------------------------------------------
+1.0 Theme Files
+--------------------------------------------------------------*/
+function am_files() {
 
-    //css files
-    wp_enqueue_style('am_main_styles', get_stylesheet_uri());
+  // FONTS
+  wp_enqueue_style('Fjalla One-font', '//fonts.googleapis.com/css?family=Fjalla+One&display=swap');
+  wp_enqueue_style('Caveat-font', '//fonts.googleapis.com/css?family=Caveat&display=swap');
+  wp_enqueue_style('Lato-font', '//fonts.googleapis.com/css?family=Lato:300,400&display=swap'); 
+
+    //CSS
+  wp_enqueue_style('am_main_styles', get_stylesheet_uri());
     
+}
+add_action('wp_enqueue_scripts', 'am_files');
+
+/*--------------------------------------------------------------
+2.0 Admin Features
+--------------------------------------------------------------*/
+
+function am_features() {
+
+  //HEADER
+  add_theme_support('title-tag');
+  add_theme_support( 'custom-logo');
+
+  //NAVIGATION
+  function register_my_menus() {
+  //registers the menus
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'extra-menu' => __( 'Extra Menu' )
+      )
+    );
   }
-  add_action('wp_enqueue_scripts', 'am_files');
+  add_action( 'init', 'register_my_menus' );
 
-  //FEATURES
-  function am_features() {
-    //wordpress dashboard features
+  //page
+  add_theme_support( 'post-thumbnails' );  
+}
+add_action('after_setup_theme', 'am_features');
 
-    //header
-    add_theme_support('title-tag');
-    add_theme_support( 'custom-logo');
+/*--------------------------------------------------------------
+3.0 Meta Tags
+--------------------------------------------------------------*/
 
-   //nav menu
-    function register_my_menus() {
-      //registers the menus
-      register_nav_menus(
-        array(
-          'header-menu' => __( 'Header Menu' ),
-          'extra-menu' => __( 'Extra Menu' )
-        )
-      );
-    }
-    add_action( 'init', 'register_my_menus' );
-
-    //page
-    add_theme_support( 'post-thumbnails' );  
-  }
-  add_action('after_setup_theme', 'am_features');
-
-  //META TAGES
-  function add_meta_tags() {
-    ?>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php }
+function add_meta_tags() {
+?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php }
   add_action('wp_head', 'add_meta_tags');
-  
-
 ?>
 
