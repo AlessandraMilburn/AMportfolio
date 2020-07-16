644abc297c79e3923595b5dd142910568982f521
Alessandra Milburn <57661880+AlessandraMilburn@users.noreply.github.com>
2020-07-16 15:45:31 +0400

    updated email details

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
 <footer>
       <div class="w-15 flex-row left-align md-w-40">
         <div class="h-100 mx-05 my-01"><a target="_blank" href="https://www.linkedin.com/in/alessandramilburn/"><i class="fab fa-linkedin-in hover-beige transition"></i></a></div>
-        <div class="h-100 mx-05 my-01"><a target="_blank" href="mailto:alessandramilburn@yahoo.com"><i class="fas fa-envelope hover-beige transition"></i></a></div>
-        <div class="h-100 mx-05 my-01"><a target="_blank" href="https://www.instagram.com/am_codesigns/"><i class="fab fa-instagram hover-beige transition""></i></a></div>
-        <div class="h-100 mx-05 my-01"><a target="_blank" href="https://github.com/AlessandraMilburn"><i class="fab fa-github hover-beige transition""></i></a></div>      
+        <div class="h-100 mx-05 my-01"><a target="_blank" href="mailto:alessandra.milburn@yahoo.com"><i class="fas fa-envelope hover-beige transition"></i></a></div>
+        <div class="h-100 mx-05 my-01"><a target="_blank" href="https://www.instagram.com/alessandra_codes/"><i class="fab fa-instagram hover-beige transition"></i></a></div>
+        <div class="h-100 mx-05 my-01"><a target="_blank" href="https://github.com/AlessandraMilburn"><i class="fab fa-github hover-beige transition"></i></a></div>      
       </div>
 
       <h4>
