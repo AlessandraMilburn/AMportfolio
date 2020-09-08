2982dfc006c91e0434faa973760bfa49c0fa1a89
Alessandra Milburn <57661880+AlessandraMilburn@users.noreply.github.com>
2020-09-08 16:23:30 +0400

    moved footer items to central column direction

--- footer.php
+++ footer.php
@@ -1,19 +1,21 @@
 </main>
 <!-------------footer---------------->
 
-<footer>
-      <div class="w-15 flex-row left-align md-w-40">
+<footer class="md-flex-column">
+      <div class="w-15 flex-row left-align md-w-100 ">
         <div class="h-100 mx-05 my-01"><a target="_blank" href="https://www.linkedin.com/in/alessandramilburn/"><i class="fab fa-linkedin-in hover-beige transition"></i></a></div>
         <div class="h-100 mx-05 my-01"><a target="_blank" href="mailto:alessandra.milburn@yahoo.com"><i class="fas fa-envelope hover-beige transition"></i></a></div>
         <div class="h-100 mx-05 my-01"><a target="_blank" href="https://www.instagram.com/alessandra_codes/"><i class="fab fa-instagram hover-beige transition"></i></a></div>
         <div class="h-100 mx-05 my-01"><a target="_blank" href="https://github.com/AlessandraMilburn"><i class="fab fa-github hover-beige transition"></i></a></div>      
       </div>
 
-      <div class="w-15 flex-row left-align md-w-40">
-        <img class="shadow-effect w-100" src="/wp-content/uploads/2020/09/icon-shopify-partner.jpg" />        
+      <div class="w-15 flex-row md-w-100">
+        <a class="w-100 h-100 flex-row md-h-50 md-w-40" target="_blank" href="https://www.shopify.com/?ref=alessandra-milburn-services&utm_campaign=amwebsite"> 
+          <img class="shadow-effect h-100 md-w-100" src="/wp-content/uploads/2020/09/icon-shopify-partner-white.png" />        
+        </a>
       </div>
 
-      <h4>
+      <h4 class="md-w-100">
         COPYRIGHT 2020 <wbr> ALESSANDRA MILBURN
       </h4>
     </footer>
