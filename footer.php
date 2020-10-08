0dff66dbf2a05641e5d760ad57da7659a05a1346
Alessandra Milburn <57661880+AlessandraMilburn@users.noreply.github.com>
2020-10-08 09:30:08 +0400

    upadated instagram link

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
       <div class="w-15 flex-row left-align md-w-100 md-center">
         <div class="h-100 mx-05 my-01"><a target="_blank" href="https://www.linkedin.com/in/alessandramilburn/"><i class="fab fa-linkedin-in hover-beige transition"></i></a></div>
         <div class="h-100 mx-05 my-01"><a target="_blank" href="mailto:alessandra.milburn@yahoo.com"><i class="fas fa-envelope hover-beige transition"></i></a></div>
-        <div class="h-100 mx-05 my-01"><a target="_blank" href="https://www.instagram.com/alessandra_codes/"><i class="fab fa-instagram hover-beige transition"></i></a></div>
+        <div class="h-100 mx-05 my-01"><a target="_blank" href="https://www.instagram.com/alessandra.codes/"><i class="fab fa-instagram hover-beige transition"></i></a></div>
         <div class="h-100 mx-05 my-01"><a target="_blank" href="https://github.com/AlessandraMilburn"><i class="fab fa-github hover-beige transition"></i></a></div>      
       </div>
 
